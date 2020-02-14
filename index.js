02f92823a8a71bd7ed0edaf42df8d217a62dce89
Vasu <tsvasu@gmail.com>
2020-02-13 23:35:46 -0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
 const mongoose = require("mongoose");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const express = require("express");
 
@@ -20,6 +24,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
