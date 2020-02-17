a2c472bf5eb17601f3bdacbb16ebcd266b9a32b6
Vasu <tsvasu@gmail.com>
2020-02-17 09:08:04 -0500

    commit

--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function() {
-  const db = process.env.vidly_db || config.get("db");
+  const db = process.env.vidly_db; //config.get("db")
+  //console.log("connecting to db " + process.env.vidly_db);
+
   mongoose
     .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    .then(() => console.log("Connected to " + config.get("db")));
+    .then(() => console.log("Connected to " + db));
 };
