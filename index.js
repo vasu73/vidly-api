87dc420e145f9b23cb418642d044dd5325982e01
Vasu <tsvasu@gmail.com>
2020-02-16 22:43:27 -0500

    save

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-require("./startup/logging")();
+//require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
